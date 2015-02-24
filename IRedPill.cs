a88a9a45e14d2a25062b954a7ef3e77d7084c1cb
SDAdham <dom.sabry@gmail.com>
2015-02-25 08:03:56 +1100

    Readify KnockKnock Test

---
+++ IRedPill.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+[assembly: ContractNamespace("http://KnockKnock.readify.net", ClrNamespace = "knockknock.readify.net")]
+namespace Readify.KnockKnock.Webservice
+{
+    [DataContract(Name = "TriangleType", Namespace = "http://KnockKnock.readify.net")]
+    public enum TriangleType : int
+    {
+
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Error = 0,
+
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Equilateral = 1,
+
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Isosceles = 2,
+
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Scalene = 3,
+    }
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract(Namespace = "http://KnockKnock.readify.net", ConfigurationName = "IRedPill")]
+    public interface IRedPill
+    {
+        [OperationContract(Action = "http://KnockKnock.readify.net/IRedPill/WhatIsYourToken", ReplyAction = "http://KnockKnock.readify.net/IRedPill/WhatIsYourTokenResponse")]
+        Guid WhatIsYourToken();
+
+        [OperationContract(Action = "http://KnockKnock.readify.net/IRedPill/FibonacciNumber", ReplyAction = "http://KnockKnock.readify.net/IRedPill/FibonacciNumberResponse")]
+        [FaultContract(typeof(System.ArgumentOutOfRangeException), Action = "http://KnockKnock.readify.net/IRedPill/FibonacciNumberArgumentOutOfRangeException" +
+        "Fault", Name = "ArgumentOutOfRangeException", Namespace = "http://schemas.datacontract.org/2004/07/System")]
+        long FibonacciNumber(long n);
+
+        [OperationContract(Action = "http://KnockKnock.readify.net/IRedPill/WhatShapeIsThis", ReplyAction = "http://KnockKnock.readify.net/IRedPill/WhatShapeIsThisResponse")]
+        TriangleType WhatShapeIsThis(int a, int b, int c);
+
+        [OperationContract(Action = "http://KnockKnock.readify.net/IRedPill/ReverseWords", ReplyAction = "http://KnockKnock.readify.net/IRedPill/ReverseWordsResponse")]
+        [FaultContract(typeof(System.ArgumentNullException), Action = "http://KnockKnock.readify.net/IRedPill/ReverseWordsArgumentNullExceptionFault", Name = "ArgumentNullException", Namespace = "http://schemas.datacontract.org/2004/07/System")]
+        string ReverseWords(string s);
+    }
+}
---
+++ RedPill.svc.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Readify.KnockKnock.Webservice
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class RedPill : IRedPill
+    {
+        public Guid WhatIsYourToken()
+        {
+            //f51694d1-5f70-4f0f-9ec5-568ea0595f22
+            //return new Guid("4659f88c-909b-4067-b35e-fd27865d5531");
+            return new Guid("f51694d1-5f70-4f0f-9ec5-568ea0595f22");
+        }
+
+        public long FibonacciNumber(long n)
+        {
+            if (n > 92)
+                throw new Exception("Fib(>92) will cause a 64-bit integer overflow");
+            long a = 0;
+            long b = 1;
+            for (long i = 0; i < n; i++)
+            {
+                long temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+        private string ReverseWord(string s)
+        {
+            char[] result = new char[s.Length];
+            Parallel.For(0, s.Length, i => { result[s.Length - 1 - i] = s[i]; });
+            return new string(result);
+        }
+
+        public string ReverseWords(string s)
+        {
+            string[] result = s.Split(' ');
+            Parallel.For(0, result.Length, i => { result[i] = ReverseWord(result[i]); });
+            return string.Join(" ", result);
+        }
+
+        public TriangleType WhatShapeIsThis(int a, int b, int c)
+        {
+            if ((a + b > c && a + c > b && b + c > a))
+            {
+                if (a == b && b == c)
+                    return TriangleType.Equilateral;
+                else if (a == b || b == c || a == c)
+                    return TriangleType.Isosceles;
+                else if (a != b && a != c && b != c)
+                    return TriangleType.Scalene;
+            }
+
+            return TriangleType.Error;
+        }
+    }
+}
